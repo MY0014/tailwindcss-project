9d68079ac0176170d60ab1d39082c5e62061d977
YesicaM <yilm1405.correo@gmail.com>
2024-04-21 05:37:55 -0600

    adding changes

--- src/App.vue
+++ src/App.vue
@@ -102,33 +102,32 @@ export default {
 
 <div class="bg-gray-200 py-96">
     <div class="container mx-auto px-4">
-      <div class="flex flex-wrap justify-center items-start px-1">
-        <!-- Contenido del div -->
-         <div class=" relative px-16 py-16 top-(-32) bottom-80 left-60 right-80 bg-gray-100 bg-opacity-100 rounded-lg">
-              <img src="/src/img/img1.png" class="w-full py-0 px-0" alt="">
-         </div>
-      </div>
-        
+        <div class="flex flex-wrap justify-center items-start px-1">
+            <!-- Contenido del div -->
+            <div class=" relative px-16 py-16 top-(-32) bottom-80 left-60 right-80 bg-gray-100 bg-opacity-100 rounded-lg">
+                <img src="/src/img/img1.png" class="w-full py-0 px-0" alt="">
+            </div>
+        </div>
+
     </div>
 </div>
-
 
 <div class="bg-orange-100 py-52">
     <div class="container mx-auto px-48 flex flex-wrap">
         <!-- Contenido del div -->
-         <!-- Columna izquierda -->
-         <div class=" relative w-full md:w-1/4 bg-white bg-opacity-40 p-40 py-10">
+        <!-- Columna izquierda -->
+        <div class=" relative w-full md:w-1/4 bg-white bg-opacity-40 p-40 py-10">
             <!-- Contenido de la columna izquierda -->
             <img src="/src/img/img4.png" class="w-full py-0 px-0" alt="img">
         </div>
-        
+
         <!-- Columna derecha -->
         <div class="w-full md:w-2/5 md:flex md:flex-col">
             <!-- Columna 1 de la derecha -->
             <div class="bg-green-200 p-20 mb-4 md:mb-0">
                 <!-- Contenido de la primera columna derecha -->
             </div>
-            
+
             <!-- Columna 2 de la derecha -->
             <div class="bg-gray-200 p-24">
                 <!-- Contenido de la segunda columna derecha -->
@@ -136,7 +135,6 @@ export default {
         </div>
     </div>
 </div>
-
 
 <div class="bg-gray-100 py-64">
     <div class="container mx-auto px-4">
@@ -150,20 +148,65 @@ export default {
     </div>
 </div>
 
-<div class="bg-gray-200 py-44">
-    <div class="container mx-auto px-4">
+<div class="bg-gray-50 py-10">
+    <div class="container mx-auto px-64">
         <!-- Contenido del div -->
+        <div class="p-10 max-w-2xl bg-gray-100 rounded-xl shadow-lg flex items-center justify-center space-x-4">
+            <div class="p-10 max-w-4xl bg-white rounded-xl shadow-lg flex items-center justify-center space-x-4">
+                <div class="shrink-0">
+                </div>
+                <div>
+                    <p class="text-orange-500 font-bold text-center">
+                        180 Days Money Back Guarantee
+                    </p>
+                    <div class="text-xl font-medium text-black text-left">
+                        <h4 class="text-2xl font-bold mb-2 px-10 ">My Amazing Headline Goes Here</h4>
+                    </div>
+                    <p class="text-slate-500">
+                        I am baby retro chillwave letterpress smal batch beard, paleo ugh banjo hot chicken godard craft beer.
+                        Biodiesel pork belly green juice VHS farm-to-table- taxidermy heirloom scenester.
+                    </p>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
-<div class="bg-orange-300 py-10">
+<div class="bg-orange-300 py-5">
     <div class="container mx-auto px-4">
         <!-- Contenido del div -->
+        <div class="flex justify-around">
+            <div class="order-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#e7e5e4" class="bi bi-shield-check" viewBox="0 0 16 16">
+                    <path d="M5.338 1.59a61 61 0 0 0-2.837.856.48.48 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.7 10.7 0 0 0 2.287 2.233c.346.244.652.42.893.533q.18.085.293.118a1 1 0 0 0 .101.025 1 1 0 0 0 .1-.025q.114-.034.294-.118c.24-.113.547-.29.893-.533a10.7 10.7 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7 7 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7 7 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56" />
+                    <path d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0" />
+                </svg>
+            </div>
+
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#e7e5e4" class="bi bi-bank" viewBox="0 0 16 16">
+                    <path d="m8 0 6.61 3h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.38l.5 2a.498.498 0 0 1-.485.62H.5a.498.498 0 0 1-.485-.62l.5-2A.5.5 0 0 1 1 13V6H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 3h.89zM3.777 3h8.447L8 1zM2 6v7h1V6zm2 0v7h2.5V6zm3.5 0v7h1V6zm2 0v7H12V6zM13 6v7h1V6zm2-1V4H1v1zm-.39 9H1.39l-.25 1h13.72z" />
+                </svg>
+
+            </div>
+
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#e7e5e4" class="bi bi-cash-coin" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8m5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0" />
+                    <path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195z" />
+                    <path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083q.088-.517.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1z" />
+                    <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 6 6 0 0 1 3.13-1.567" />
+                </svg>
+            </div>
+        </div>
+
     </div>
 </div>
 
-<div class="bg-teal-800 py-40">
+<div class="bg-teal-900 py-5">
     <div class="container mx-auto px-4">
-        <!-- Contenido del div -->
+        <!-- Contenido del div --><img src="/src/img/img5.png" class="w-full py-0 px-0" alt="">
+        <p>hola</p>
     </div>
 </div>
 </template>
